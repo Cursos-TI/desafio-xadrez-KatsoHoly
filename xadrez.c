d0c44f3e4e8501c4c41562b904113fe0cad358a3
KatsoHoly <douglas.lcavalcanti@gmail.com>
2025-04-27 23:42:10 +0000

    Incluindo o cavalo no tabuleiro

--- xadrez.c
+++ xadrez.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int torre, bispo = 0, rainha = 0;
+    int torre, bispo = 0, rainha = 0, cavalo;
 
     // //Mover a Torre 5 vezes para cima
     printf("Movimento da Torre:\n");
@@ -10,18 +10,30 @@ int main() {
     }
 
     // //Mover o Bispo 5 vezes na diagonal para cima e direita
-    printf("\nMovimento do Bispo:\n");
+    printf("Movimento do Bispo:\n");
     while (bispo < 5) {
         printf("Cima Direita\n"); // Diagonal para cima e direita
         bispo++;
     }
 
     // //Mover a Rainha 8 vezes para a esquerda
-    printf("\nMovimento da Rainha:\n");
+    printf("Movimento da Rainha:\n");
     do {
         printf("Esquerda\n"); // Esquerda 8 vezes
         rainha++;
     } while (rainha < 8);
+
+    printf("Movimento do Cavalo\n");
+    while (cavalo--)
+    {
+        for(int cavalo = 0; cavalo < 2; cavalo++){
+            printf("Esquerda\n"); //Esquerda 2 vezes
+        }
+            printf("Cima\n"); //Cima 1 vez
+    }   
+        
+
+
 
     return 0;
 }
